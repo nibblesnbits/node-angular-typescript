003b05cddb28ffc15add2574ddfdcb04e8738110
Stephen Collins <stephen@glimpselive.com>
2015-09-10 14:02:42 -0400

    fixed protractor tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,7 @@ exports.config = {
     framework: 'jasmine2',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     baseUrl: 'http://localhost:3000',
-    specs: ['test/e2e/*.spec.js'],
+    specs: [ 'test/e2e/*.spec.js' ],
     onPrepare: function() {
     },
     allScriptsTimeout: 110000,
