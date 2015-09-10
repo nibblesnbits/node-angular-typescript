2491130ea8c408c8f14d3ce244b88be4955b6f22
Stephen Collins <stephen@glimpselive.com>
2015-09-10 14:16:55 -0400

    added grunt tasks for tests

--- gruntfile.js
+++ gruntfile.js
@@ -1,23 +1,54 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({
     ts: {
-      default : {
-        src: ["**/*.ts", "!node_modules/**/*.ts", '!typings/**/*.ts'],
-        // options: {
-        //   declaration: true
-        // }
+      default: {
+        src: ["**/*.ts", "!node_modules/**/*.ts", '!typings/**/*.ts']
       }
     },
     watch: {
-        src: ["**/*.ts", "!node_modules/**/*.ts"],
+      src: ["**/*.ts", "!node_modules/**/*.ts"],
       tasks: ['ts']
+    },
+    protractor: {
+      options: {
+        configFile: "protractor.conf.js",
+        keepAlive: false, // If false, the grunt process stops when the test fails. 
+        noColor: false,
+        args: {
+
+        }
+      },
+      default: {
+        options: {
+          configFile: "protractor.conf.js",
+          args: {}
+        }
+      },
+    },
+    protractor_webdriver: {
+      default: {
+        options: {
+          path: 'node_modules/.bin',
+          command: 'webdriver-manager start',
+        },
+      },
+    },
+    karma: {
+      default: {
+        configFile: 'karma.conf.js',
+        singleRun: true,
+        browsers: ['PhantomJS']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-ts');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-protractor-runner');
+  grunt.loadNpmTasks('grunt-protractor-webdriver');
+  grunt.loadNpmTasks('grunt-karma');
 
   grunt.registerTask('default', ['watch']);
-  
+  grunt.registerTask('test', ['karma', 'protractor_webdriver', 'protractor']);
 };
