5269e08ff1de8f84682cb22423ebb0b002c7e493
Stephen Collins <stephen@glimpselive.com>
2015-10-23 16:44:07 -0400

    added more e2e tests; Selenium is being dumb

--- test/e2e/home.spec.js
+++ test/e2e/home.spec.js
@@ -1,22 +1,45 @@
-// var httpBackendMock = function() {
-//     angular.module('mockBackend', ['ngMockE2E'])
-//     .value('configData', arguments[0])
-//     .run(function($httpBackend,configData) {
-//         console.log('bootstrapped!');
-//         $httpBackend.whenGET(/\/api.*/).respond(configData.fakeData);
-//     });
-// };
-// 
-// var config = {
-// 	fakeData: [
-// 		{foo: 'bar'}
-// 	]
-// };
+var httpBackendMock = function() {
+    angular.module('mockBackend', ['ngMockE2E'])
+    .value('configData', arguments[0])
+    .run(function($httpBackend,configData) {
+        console.log('bootstrapped!');
+		
+		// this is because Selenium is dumb about how I store my configs.
+        $httpBackend.whenGET("null/data").respond(configData.fakeData);
+        $httpBackend.whenGET(/.*\.html/).passThrough();
+    });
+};
+
+var config = {
+	fakeData: [
+		{foo: 'bar'}
+	]
+};
+
+describe('app', function() {
+	beforeEach(function() {
+		browser.addMockModule('mockBackend', httpBackendMock, config);
+	});
+
+	it('should set items in localStorage', function() {
+		browser.get('/');
+		
+		// TODO: for some reason Selenium doesn't like the way I use providers.
+		
+		var value = browser.executeScript("return window.localStorage.getItem('config_dataApiUrl');");
+		
+		console.log('url: ' + value);
+		
+		// browser.pause();
+		
+		expect(value).toBe("/api");
+	});
+});
 
 describe('home page', function() {
-	// beforeEach(function() {
-	// 	browser.addMockModule('mockBackend', httpBackendMock, config);
-	// });
+	beforeEach(function() {
+		browser.addMockModule('mockBackend', httpBackendMock, config);
+	});
 	
 	it('should have a "homePage" element', function() {
 		browser.get('/');
@@ -32,7 +55,16 @@ describe('home page', function() {
 			
 			var e = element.all(by.repeater('d in vm.data'));
 			
-			expect(e.count()).toBe(1);
+			expect(e.count()).toBe(config.fakeData.length);
+		});
+	});
+	
+	afterAll(function() {
+		browser.manage().logs().get('browser').then(function(browserLogs) {
+		// browserLogs is an array of objects with level and message fields
+			browserLogs.forEach(function(log){
+				console.log('Browser Console Log: ' + log.message);
+			});
 		});
 	});
 });
